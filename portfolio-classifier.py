400d527e634d6dab0401dc7f554ddb3d1e92b7ad
fizban99 <13463439+fizban99@users.noreply.github.com>
2023-01-02 20:18:15 +0100

    Add domain parameter

--- portfolio-classifier.py
+++ portfolio-classifier.py
@@ -14,12 +14,6 @@ from bs4 import BeautifulSoup
 import os
 import json
 
-# Modify this to the morningstar domain where your securities can be found
-# e.g. es for spain, de for germany, fr for france...
-# this is only used to find the corresponding secid from the ISIN
-DOMAIN = 'de'
-
-
 
 requests_cache.install_cache(expire_after=86400) #cache downloaded files for a day
 requests_cache.remove_expired_responses()
@@ -676,8 +670,15 @@ if __name__ == '__main__':
                  "For each security, you need to have an ISIN"])
     )
 
+    # Morningstar domain where your securities can be found
+    # e.g. es for spain, de for germany, fr for france...
+    # this is only used to find the corresponding secid from the ISIN
+    parser.add_argument('-d', default='es',  dest='domain', type=str,
+                        help='Morningstar domain from which to retrieve the secid (default: es)')
+    
     parser.add_argument('input_file', metavar='input_file', type=str,
                    help='path to unencrypted pp.xml file')
+    
     parser.add_argument('output_file', metavar='output_file', type=str, nargs='?',
                    help='path to auto-classified output file', default='pp_classified.xml')
 
@@ -686,6 +687,7 @@ if __name__ == '__main__':
     if "input_file" not in args:
         parser.print_help()
     else:
+        DOMAIN = args.domain
         Isin2secid.load_cache()
         pp_file = PortfolioPerformanceFile(args.input_file)
         for taxonomy in taxonomies:
